9d3cfe6eec4ea114ecaf880c1610e404114e140f
Bakhtin Igor <igorbakhtin1@gmail.com>
2020-08-01 22:02:26 +0300

    Create blog

---
+++ blog/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from blog.models import *
+
+
+class Home(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 3
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Стримерский уголок'
+        return context
+
+
+def index(request):
+    return render(request, 'blog/index.html')
+
+
+def get_category(request, slug):
+    return render(request, 'blog/category.html')
+
+def get_post(request, slug):
+    return render(request, 'blog/category.html')
